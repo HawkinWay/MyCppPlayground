417b7738b3a39097932413655e13f0154bd3d06d
ShovyInLinux <1826784860@qq.com>
2026-04-11 17:30:07 +0800

    review and add annotations

--- CppNotes/C++11/std_move.cpp
+++ CppNotes/C++11/std_move.cpp
@@ -2,6 +2,9 @@
 #include<cstring>
 #include<cstdint>
 
+
+// 有名字的就是左值，没名字的就是右值
+
 class String {
 private:
     char* m_Data;
@@ -9,7 +12,7 @@ private:
 public:
     String() = default;
     String(const char* string) {
-        printf("String(const char* string), Created!\n");   // 1
+        printf("String(const char* string), Created!\n");   // 1 String("原神"), 产生一个匿名临时对象（右值）,堆上分配了一块内存，存放着“原神”。
         m_Size = strlen(string);
         m_Data = new char[m_Size];
         memcpy(m_Data, string, m_Size);
@@ -23,7 +26,7 @@ public:
     }
     
     String(String&& other) {
-        printf("String(String&& other), Moved!\n");     // 2
+        printf("String(String&& other), Moved!\n");     // 2 临时对象被作为参数传递给 name
         m_Size = other.m_Size;
         m_Data = other.m_Data;
         other.m_Data = nullptr;
@@ -31,7 +34,7 @@ public:
     }
  
    ~String() {
-        printf("~Sring(), Destroyed!\n");       // 4    6
+        printf("~Sring(), Destroyed!\n");       // 4 那个匿名的临时对象（原本叫 name 的那个）生命周期结束了    6 对象 entity 被销毁
         delete[] m_Data;
     }
  
@@ -54,8 +57,9 @@ public:
     //         std::cout << "Entity(String&& name).\n";
     // }
     Entity(String&& name) : m_Name(std::move(name)) {   // 3 调用的是m_Name((String&&)name),将左值转换为右值
-            std::cout << "Entity(String&& name).\n";
-    }
+            std::cout << "Entity(String&& name).\n";    // 这里虽然类型是右值引用，但因为它有名字 name ，它在语法上被视为左值，直接m_Name(name)依然会调用拷贝构造函数（深拷贝）。
+    }                                                   // 加上 std::move(name) 后，它被转换回右值，触发 String(String&& other)。
+            
     void PrintName() {
         m_Name.Print();     // 5
     }
@@ -71,17 +75,19 @@ int main(){
     int b = a;      // a and b are both lvalue
 
     int m = 10;
-    //int &&n = m;   // wrong
-    //int &n = 10;   // wrong
+    //int &&n = m;   // wrong, rvalue reference can't be bound to lvalue
+    //int &n = 10;   // wrong, lvalue reference can't be bound to rvalue
     int &&n = 11;
     n = 100;
     std::cout << "\nn: " << n << std::endl;
     
-    //int &j = 10;  //wrong,左值引用不能绑定右值
+    //int &j = 10;      //wrong,左值引用不能绑定右值
     const int &j = 10;  //const左值引用可以绑定右值
                         //这会创建临时对象，相当于  int temp = 10;     const int &j = temp;
     
     std::string s = "hello";
-    std::string t = std::move(s);    //std::move()是xvalue，属于右值的一种。移动后s仍然是有效对象 但处于未指定状态（不一定为空）
-
+    std::string b = s;      // 拷贝：b 申请了新内存，复制了 s 的内容
+    std::string t = std::move(s);    // t 直接接管 s 的原本拥有的资源(如堆内存指针)，s 变为有效但未指定的状态
+    
+    //std::move()是xvalue，属于右值的一种。移动后s仍然是有效对象 但处于未指定状态（不一定为空）
 }
