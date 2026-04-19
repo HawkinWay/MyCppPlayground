c6bd7b40166eeb7f381ac6e16cb0756a674c7cb4
HawkinWay <whq2006@whq2026deMacBook-Air.local>
2026-04-19 16:06:48 +0800

    different uses of static between class members and non-class members

--- CppNotes/static.cpp
+++ CppNotes/static.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 
 // static 自由函数具有内部链接（Internal Linkage）,不会被其他翻译单元(Translated Unit)看到
-// 尽量在.cpp中使用static以减少符号污染，在.h中使用static function会导致包含它的每一个翻译单元都有一个独立副本
+// 对于非类成员，尽量在.cpp中使用static以减少符号污染，在.h中使用static function会导致包含它的每一个翻译单元都有一个独立副本
 // 惰性初始化(Static Lazy Initialization): 在 C++11 及更高版本中，在函数内部声明的静态局部变量是线程安全且惰性初始化的。只有当代码执行到该声明行时，变量才会被创建。
 
 class Entity{
