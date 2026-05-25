c0c76b0ceefdf9687614510fbebcae564ef8c355
HawkinWay <1826784860@qq.com>
2026-05-25 17:44:08 +0800

    add nothing

--- CppNotes/C++11/noexcept.cpp
+++ CppNotes/C++11/noexcept.cpp
@@ -1,7 +1,10 @@
 // a specifier, a suffix to a function declaration
 // Every function(include constructor and destructor) can be postfixed with 'noexcept', compiler will ensure this function won't throw excrption
 // This means no exceptions can escape from their scope of function.
-// If a 'noexcept' function throw out an exception, there are two situations: 1.The exception was caught within the function scope， then it cannot be rethrown.  2. The exception was not caught within the function scope, Cpp program calls 'std::terminate()' and end the program.
+
+// If a 'noexcept' function throw out an exception, there are two situations: 
+// 1.The exception was caught within the function scope， then it cannot be rethrown.  
+// 2. The exception was not caught within the function scope, Cpp program calls 'std::terminate()' and end the program.
 
 
 // WHEN TO USE?
